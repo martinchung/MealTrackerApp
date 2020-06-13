8e36035320ee98ce13afd3ba99976adc51426f66
martin <martin@martinchung.com>
2020-06-13 12:18:35 -0700

    Initial Commit

---
+++ SiWCApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SiWCApp
+//
+//  Created by Martin Chung on 2020-06-13.
+//  Copyright © 2020 Martin Chung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
